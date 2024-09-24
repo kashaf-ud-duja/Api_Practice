6f99d9798d8fdb3f86bcd66210350eee63b3247d
kashaf-ud-duja <tahirkashaf7@gmail.com>
2024-09-24 18:12:34 +0500

    User API

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:api_practice/custom_screen.dart';
 import 'package:api_practice/home_screen.dart';
+import 'package:api_practice/user_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const CustomScreen(),
+      home: const UserScreen(),
       debugShowCheckedModeBanner:false,
     );
   }
---
+++ lib/user_screen.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+
+import 'package:api_practice/Models/user_modal.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+class UserScreen extends StatefulWidget {
+  const UserScreen({super.key});
+
+  @override
+  State<UserScreen> createState() => _UserScreenState();
+}
+
+class _UserScreenState extends State<UserScreen> {
+List<UserModal> usersList = [];
+Future<List<UserModal>> GetUserAPI() async{
+final response =await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+var data = jsonDecode(response.body.toString());
+
+if (response.statusCode == 200) {
+for (Map<String , dynamic> i in data) {
+usersList.add(UserModal.fromJson(i));
+}
+
+  return usersList;
+}else{
+  return usersList;
+}
+
+
+}
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+            appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+        title: const Text(
+          "API Practice",
+          textAlign: TextAlign.center,
+          selectionColor: Colors.white,
+        ),
+      ),
+      body: column,
+    );
+  }
+}
